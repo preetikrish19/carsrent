b4fad624dfff114278351909f9dcc944313e77d5
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2021-03-07 18:05:40 +0530

    sql

---
+++ admins/addpassenger.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if(isset($_POST['email'])) {
+    include 'db.php';
+    $name = $con->real_escape_string( $_POST['name']);
+    $phone = $_POST['phone'];
+    $age = $_POST['age'];
+    $pwd = $con->real_escape_string( $_POST['pwd']);
+    $email = $con->real_escape_string($_POST['email']);
+
+    $query = "INSERT INTO passengers (name, phone, pwd, age, email) VALUES ('$name', $phone, '$pwd', $age, '$email')";
+    if ($con->query($query)) {
+        echo "SUCCESS";
+    } else {
+        echo $con->error;
+    }
+}else{
+    echo "NOT VIEWABLE";
+}
